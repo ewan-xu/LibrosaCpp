1d5c56b48b8de1b8b265399e4e96ea3d050639bc
徐潇宇 <xuxiaoyu@lizhi.fm>
2020-12-23 20:18:13 +0800

    add mfcc

--- test/wavreader.h
+++ test/wavreader.h
@@ -16,22 +16,22 @@
 * -------------------------------------------------------------------
 */
 
-#ifndef WAVREADER_H
-#define WAVREADER_H
+#ifndef WAVREADER_H_
+#define WAVREADER_H_
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-	void* wav_read_open(const char *filename);
-	void wav_read_close(void* obj);
+void* wav_read_open(const char *filename);
+void wav_read_close(void* obj);
 
-	int wav_get_header(void* obj, int* format, int* channels, int* sample_rate, int* bits_per_sample, unsigned int* data_length);
-	int wav_read_data(void* obj, unsigned char* data, unsigned int length);
+int wav_get_header(void* obj, int* format, int* channels, int* sample_rate, int* bits_per_sample, unsigned int* data_length);
+int wav_read_data(void* obj, unsigned char* data, unsigned int length);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif
-
